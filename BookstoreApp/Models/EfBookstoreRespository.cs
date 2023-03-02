e1a7a2510b516660823e205cc0d29cbbc1b23639
Carson Dudley <cdudley6@byu.edu>
2023-03-01 19:52:49 -0700

    Add project files.

---
+++ BookstoreApp/Models/EfBookstoreRespository.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookstoreApp.Models
+{
+    public class EfBookstoreRespository : IBookstoreRepository
+    {
+        private BookstoreContext context { get; set; }
+
+        public EfBookstoreRespository (BookstoreContext temp)
+        {
+            context = temp;
+        }
+        public IQueryable<Book> Books => context.Books;
+    }
+}
